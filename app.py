5b8731484c4a010b51d5b2ce93198127180daee5
MusarratDiya <afra.musarrat.diya@g.bracu.ac.bd>
2025-08-27 16:28:46 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -813,4 +813,5 @@ if __name__ == '__main__':
         username = input("Enter username to make admin: ")
         make_user_admin(username)
     else:
-        app.run(debug=True)
+        port = int(os.getenv('PORT', 5000))
+        app.run(host='0.0.0.0', port=port, debug=False)
